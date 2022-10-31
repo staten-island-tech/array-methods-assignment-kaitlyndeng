dc15368a7a4946ac0db13a0796e1bdb60e24e067
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-10-31 10:14:44 -0400

    Arrays Commit 2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(connected);
+const cats = [
+  {name: "Angela", type: "siamese", size: "small", age: 3},
+
+  },
+  "ragdoll",
+  "munchkin",
+];
+
+console.log(cats[0]);
+cats.forEach((cats) => {
+
+});
